3229078f0be007ab6c21d5ee31e352e57cbaffc9
Chris Ertel <chris@kedagital.com>
2016-05-27 02:06:16 -0500

    Add test helper for generating test emails.
    This prevents a particular hardcoded hack from being needed.

--- test/actions/signin_user_test.exs
+++ test/actions/signin_user_test.exs
@@ -7,21 +7,23 @@ defmodule PhoenixLobsterTest.Actions.SiginUserTest do
   @tag :user
   @tag :authentication
   test "Signin user should return user" do
-    {:ok, user} = add_user("tony3@stark.com", password: "iamironman")
+    email = gen_fake_email( "tony" )
+    {:ok, user} = add_user(email, password: "iamironman")
 
-    {:ok, signedin_user} = SigninUser.execute("tony3@stark.com", "iamironman")
+    {:ok, signedin_user} = SigninUser.execute(email, "iamironman")
     
     assert signedin_user.id == user.id
-    assert signedin_user.email == "tony3@stark.com"
+    assert signedin_user.email == email
   end
 
   @tag :actions
   @tag :user
   @tag :authentication
   test "Signin user should return error if password is wrong" do
-    {:ok, _} = add_user("tony2@stark.com", password: "iamironman")
+    email = gen_fake_email( "tony" )
+    {:ok, user} = add_user(email, password: "iamironman")
 
-    {:error, message} = SigninUser.execute("tony2@stark.com", "wrongpassword")
+    {:error, message} = SigninUser.execute( email, "wrongpassword")
     
     assert message == "Email and/or password invalid"
   end
