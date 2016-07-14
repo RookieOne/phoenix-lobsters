cda4352cd717bd770412d84bda854c8b68e05c88
Chris Ertel <chris@kedagital.com>
2016-07-14 15:59:46 -0500

    Fix not setting parent comments on comment submission.

--- test/actions/create_comment_on_story_test.exs
+++ test/actions/create_comment_on_story_test.exs
@@ -22,4 +22,22 @@ defmodule PhoenixLobstersTest.Actions.CreateCommentOnStory do
     assert user.id == comment.user_id
     assert story.id == comment.story_id
   end
+
+  @tag :actions
+  @tag :comment
+  test "Users can create a comment on a comment on a story", %{user: user, story: story} do
+    test_markdown = "# Test"
+    test_reply_markdown = "# Reply"
+    test_html = "<h1>Test</h1>"
+    test_reply_html = "<h1>Reply</h1>"
+    {:ok, comment} = CreateCommentOnStory.execute( user.id, test_markdown, story.id)
+    {:ok, replyComment} = CreateCommentOnStory.execute( user.id, test_reply_markdown, story.id, comment.id)
+
+    # other assertions here
+    assert test_reply_html == replyComment.compiled_html |> String.strip
+    assert test_reply_markdown == replyComment.uncompiled_markdown
+    assert comment.id == replyComment.comment_id
+    assert user.id == replyComment.user_id
+    assert story.id == replyComment.story_id
+  end
 end
--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -2,14 +2,14 @@ defmodule PhoenixLobsters.CommentController do
   use PhoenixLobsters.Web, :controller
   alias PhoenixLobsters.Actions.CreateCommentOnStory
 
-  def create(conn, %{ "comment" => %{ "content" => content, "story_id" => story_id } }) do
+  def create(conn, %{ "comment" => %{ "content" => content, "story_id" => story_id, "comment_id" => comment_id } }) do
     case conn |> get_session(:current_user) do
       nil ->
         conn
         |> put_flash(:error, "You must be logged in to submit comments.")
         |> redirect(to: "/stories/#{story_id}")
       user_id ->
-        case CreateCommentOnStory.execute(user_id, content, story_id) do
+        case CreateCommentOnStory.execute(user_id, content, story_id, comment_id) do
           {:ok,comment} ->
             conn
             |> redirect(to: "/stories/#{story_id}")
@@ -19,5 +19,9 @@ defmodule PhoenixLobsters.CommentController do
             |> redirect(to: "/stories/#{story_id}")
         end    
     end
+  end
+
+  def create(conn, %{ "comment" => %{ "content" => content, "story_id" => story_id} }) do
+    create(conn, %{ "comment" => %{ "content" => content, "story_id" => story_id, "comment_id" => nil } })
   end
 end
